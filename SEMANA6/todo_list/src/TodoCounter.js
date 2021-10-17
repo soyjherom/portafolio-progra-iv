584b483ed9b918e1c50a43379cb2a91ee4e74c52
soyjherom <jherom@tecnica.co.cr>
2021-10-17 12:13:34 -0600

    [SEMANA 6] Agregando estilos como constantes

--- SEMANA6/todo_list/src/TodoCounter.js
+++ SEMANA6/todo_list/src/TodoCounter.js
@@ -1,7 +1,11 @@
 import React from 'react';
+const estilos = {
+    color:'red', 
+    backgroundColor:'yellow'
+};
 function TodoCounter(){
     return (
-        <h2>Has completado 2 de 3 tareas</h2>
+        <h2 style={estilos}>Has completado 2 de 3 tareas</h2>
     );
 }
 export { TodoCounter };
