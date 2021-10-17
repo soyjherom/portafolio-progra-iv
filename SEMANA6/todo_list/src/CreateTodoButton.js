f2bc42a6087a2e0a985a8fbb3497b0d552880de6
soyjherom <jherom@tecnica.co.cr>
2021-10-17 12:48:21 -0600

    [SEMANA 6] Agregando estilos al boton

--- SEMANA6/todo_list/src/CreateTodoButton.js
+++ SEMANA6/todo_list/src/CreateTodoButton.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import './CreateTodoButton.css'
 function CreateTodoButton(){
     return(
-        <button>+</button>
+        <button className="CreateTodoButton">+</button>
     );
 }
 export {CreateTodoButton};
