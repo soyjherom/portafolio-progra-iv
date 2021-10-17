d2f4dbc1f92b3a63caf296bb275a53f6b641f1e8
soyjherom <jherom@tecnica.co.cr>
2021-10-17 12:15:12 -0600

    [SEMANA 6] Estilos en linea

--- SEMANA6/todo_list/src/TodoCounter.js
+++ SEMANA6/todo_list/src/TodoCounter.js
@@ -1,11 +1,9 @@
 import React from 'react';
-const estilos = {
-    color:'red', 
-    backgroundColor:'yellow'
-};
 function TodoCounter(){
     return (
-        <h2 style={estilos}>Has completado 2 de 3 tareas</h2>
+        <h2 style={{color:'red', backgroundColor:'yellow'}}>
+            Has completado 2 de 3 tareas
+        </h2>
     );
 }
 export { TodoCounter };
