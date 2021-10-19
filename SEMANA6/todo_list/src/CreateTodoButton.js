3bbe07a6c2e17c48d4c689314cb8d60271a439b2
soyjherom <jherom@tecnica.co.cr>
2021-10-19 14:34:57 -0600

    [SEMANA 6] Enviando parametros a una funcion como una constante

--- SEMANA6/todo_list/src/CreateTodoButton.js
+++ SEMANA6/todo_list/src/CreateTodoButton.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './CreateTodoButton.css'
 function CreateTodoButton(props){
-    const onClickButton = () => {
-        alert("Hola mundo")
+    const onClickButton = (mensaje) => {
+        alert(mensaje)
     }
     return(
         <button 
             className="CreateTodoButton"
-            onClick={onClickButton}
+            onClick={() => onClickButton("This is a parameter")}
         >+</button>
     );
 }
