d11c73f848dbc6eb3c5bf2b140db42dca8d94254
soyjherom <jherom@tecnica.co.cr>
2021-10-26 17:02:09 -0600

    [SEMANA 6]Eliminando ejemplo innecesario

--- SEMANA6/todo_list/src/TodoSearch.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import './TodoSearch.css'
-function TodoSearch(){
-    return(
-        <input className="TodoSearch" placeholder="Algo que hacer"/>
-    );
-}
-export { TodoSearch };
